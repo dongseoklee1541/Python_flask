50c319013c71a076294defa58dd628fdad636ba3
dongseoklee <41297473+dongseoklee1541@users.noreply.github.com>
2020-01-03 17:35:09 +0900

    The Flask Mega-Tutorial Part 4
    login form complete

---
+++ microblog/app/routes.py
@@ -0,0 +1,66 @@
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user,login_user,logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+	posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+	return render_template("index.html", title='Home Page', posts=posts)
+	
+@app.route('/login', methods=['GET','POST'])
+def login():
+	#로그인한 user가 login 창으로 들어오면 index로 돌려보낸다.
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+		
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		# 전체 db 에서 username이 일치하는 것들만 가져온다
+		if user is None or not user.check_password(form.password.data):
+			flash('invalid username or password')
+			return redirect(url_for('login'))
+			# id or password 가 틀렸을 경우 메시지와 login으로 돌려보낸다.
+		login_user(user, remember=form.remember_me.data)
+		
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '': # check
+			next_page = url_for('index')
+			return redirect(next_page)
+		return redirect(url_for('index'))
+	
+	return render_template('login.html',title='Sign In',form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+	
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
