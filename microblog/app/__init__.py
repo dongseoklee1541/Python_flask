9b7e7b480c35c5346f1dafd52f0761fc32a147fc
DongSeok Lee/이동석 <41297473+dongseoklee1541@users.noreply.github.com>
2020-01-08 20:05:11 +0900

    The Flask Mega-Tutorial Chapter 13

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,25 +1,28 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel, lazy_gettext as _l
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_bootstrap import Bootstrap
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app) # db : database
 migrate = Migrate(app,db)
 momnent = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to aceess this page.')
 mail = Mail(app)
-bootstrap = Bootstrap(app)
 
 if not app.debug: # running without debug mode
 	if app.config['MAIL_SERVER']:
@@ -51,5 +54,9 @@ if not app.debug: # running without debug mode
 	app.logger.setLevel(logging.INFO)
 	app.logger.info('Microblog startup')
 
+@babel.localeselector
+def get_locale():
+	#return request.accept_languages.best_match(app.config['LANGUAGES'])
+	return 'es'
 
 from app import routes,models,errors # to avoid reciprocal imports 'app'
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,11 +1,20 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
+from flask_babel import _, get_locale
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+    g.locale = str(get_locale())
+
 
 @app.route('/reset_password_request',methods=['GET','POST'])
 def reset_password_request():
@@ -34,12 +43,6 @@ def reset_password(token):
 		flash('Your password has been reset.')
 		return redirect(url_for('login'))
 	return render_template('reset_password.html',form=form)
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
