53e09ed2cc226912b4aec2aced6cef3cb1875f40
DongSeok Lee/이동석 <41297473+dongseoklee1541@users.noreply.github.com>
2020-01-08 13:32:07 +0900

    The Flask Mega-Tutorial Chapter 11

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Python_flask
+# Welcome to Microblog!
 
-Study with flask mega tutorial
-
-https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
-
+This is an example application featured in my [Flask Mega-Tutorial](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world). See the tutorial for instructions on how to work with it.
---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+SQLALCHEMY_DATABASE_URI = location of the app's db
+"""
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-nerver-guess'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['your-email@example.com'] # 오류보고서를 수신할 이메일 주소 목록
+	POSTS_PER_PAGE = 25
